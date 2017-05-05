2afd374aa0ed5ae53570434de5d023a2e357dcfa
DavidChi <DavidChi>
2017-05-05 23:54:32 +0800

    Homeworks on wk3

--- main.js
+++ main.js
@@ -103,9 +103,124 @@ function genLine(){
         });
 }
 
+function genTaipeiBar(){
+    var filepath = 'invoice-taipei.json',
+        s = d3.select('#svg_c05_hw1');
+    d3.json(filepath, function(dataset){
+        var fData = dataset.filter(function(d){
+            return d.amount>1000000000 && d.date==='2016/8/1' && d.cid==='A';//(d.city ==='台北市' || d.city==='臺北市');
+        });
+        
+        var xScale = d3.scale.linear().domain([100000000,10000000000]).range([0,100]);
+        
+        for(var i in fData){
+            s
+                .append('text')
+                .attr({
+                    'x': 10,
+                    'y': 20+i*20,
+                    'font-size': 15
+                })
+                .text(fData[i].industry);
+            s
+                .append('rect')
+                .attr({
+                    'x': 250,
+                    'y': 10+i*20,
+                    'height': 10,
+                    'width': xScale(fData[i].amount),//fData[i].amount/100000000,
+                    'fill': 'red'
+                });
+        }
+    });
+    /*
+    //d3.csv(filepath, preProcess, function(dataset){...};
+    function preProcess(d){
+        //convert string to number...
+    }
+    */
+}
+
+function genScore(arr){
+    var arr = [85,60,99,49,77,82],
+        txt = d3.select('#atc_c06_hw1'),
+        s,
+        p = 100,
+        h = 300,
+        w = 800;
+    txt
+        .selectAll('div')
+        .data(arr)
+        .enter()
+        .append('div');
+    txt
+        .selectAll('div')
+        .data(arr)
+        .exit()
+        .remove();
+    txt
+        .selectAll('div')
+        .text(function(d,i){return Number(i)+1+':'+d;})
+        .style({'color':function(d){return d<70?'red':'black';}});
+    
+    var arr2 = arr;
+    d3.select('#atc_c06_hw2').append('svg').attr({width:w, height:h, id:'svg_c06_hw2'});
+    s = d3.select('#svg_c06_hw2');
+    
+    function bind(){
+        var selection = s.selectAll('rect').data(arr2);
+        selection.enter().append('rect');
+        selection.exit().remove();
+        var s_txt = s.selectAll('text').data(arr2);
+        s_txt.enter().append('text');
+        s_txt.exit().remove();
+    }
+    function render(){
+        s.selectAll('rect').attr({
+             x: function(d,i){
+                return p+(40+2)*i+10;
+            },
+            y: function(d,i){
+                return h-p-d;
+            },
+            width: 40,
+            height: function(d,i){return d;},
+            fill: function(d,i){
+                if(d<70){return 'red';}
+                else{return 'lightgreen';}
+            }
+        });
+        s.selectAll('text').attr({
+            x: function(d,i){
+                return p+(40+2)*i+10;
+            },
+            y: function(d,i){
+                return h-p+20;
+            }
+        }).text(function(d){return d;});
+    }
+    function update_add(){
+        var num = random(10,100);
+        arr2.push(num);
+        bind();
+        render();
+    }
+    function update_rm(){
+        arr2.shift();
+        bind();
+        render();
+    }
+    $('#ipt_c06_hw2_1').on('click', update_add);
+    $('#ipt_c06_hw2_2').on('click', update_rm);
+    bind();
+    render();
+}
+
 $(function(){
     genStaff();
     genBar();
     genLine();
     $('#btn_c04_hw1').on('click', launch);
+    genTaipeiBar();
+    genScore();
 })
